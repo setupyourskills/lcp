06d648ddb30ee477444c923516c26b529cf39bba
Setup Your Skills <setupyourskills@gmail.com>
2026-01-12 23:55:34 +0800

    feat: create ColorCard component

---
+++ app/components/component/ColorCard.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+div.color-card-component
+  p.color-card-component__color-name {{ props.colorName }}
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  colorName: {
+    type: String,
+    default: "Color name"
+  }
+})
+</script>
+
+<style lang="sass">
+.color-card-component
+  background-color: $element-background-color
+  border-radius: 25px
+  box-shadow: 4px 4px 4px 0px $secondary
+
+  &__color-name::before
+    content: ''
+    display: block
+    margin-inline: auto
+    margin-block: $phi1
+    width: $phi3
+    aspect-ratio: 1 / 1;
+    background-color: v-bind("props.colorName")
+    border-radius: 50%
+
+  &__color-name
+    margin-block: 0 $phi1
+</style>
