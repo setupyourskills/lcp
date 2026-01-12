8f7ffeefb909e1c237662ae8b15fc3a2118676a8
Setup Your Skills <setupyourskills@gmail.com>
2026-01-12 23:13:51 +0800

    feat(Features): add content to the section

--- app/components/section/Features.vue
+++ app/components/section/Features.vue
@@ -1,4 +1,24 @@
 <template lang="pug">
 section.features
-  h1 features
+  div.features__container
+    ComponentInfoCard.features__info-card
+      template(#title) Couleurs
+      template(#content) Des couleurs #[span.font-bold vibrantes] qui embélissent vos œuvres et #[span.font-bold résistent] à l'épreuve du temps.
+
+    ComponentInfoCard.features__info-card
+      template(#title) Compatible
+      template(#content) Que vous utilisiez un laser Diode, CO2 ou Fibre, nos poudres sont conçues pour fonctionner #[span.font-bold sur la majorité des machines.]
+
+    ComponentInfoCard.features__info-card
+      template(#title) Adaptable
+      template(#content) Nos poudres peuvent adhérer à #[span.font-bold tous types de matériaux] (bois, métal, verre, plastique...), laissez parler votre créativité.
 </template>
+
+<style lang="sass" scoped>
+.features
+
+  &__container
+    display: flex
+    flex-direction: column
+    gap: $phi1_5
+</style>
