52d3ee57e77bb585cfb452a02d134a8f036d4564
Setup Your Skills <setupyourskills@gmail.com>
2026-01-12 19:03:48 +0800

    chore: initialize project repository and basic setup

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/image", "@nuxt/fonts"],
+  css: ["normalize.css/normalize.css"],
+  image: {
+    dir: "assets/images",
+  },
+  fonts: {
+    families: [
+      {
+        name: "Source Sans 3",
+        provider: "google",
+      },
+    ],
+    defaults: {
+      weights: [400, 700],
+      styles: ["normal", "italic"],
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: `
+            // @use "~/assets/styles/variables.sass" as *;
+            // @use "~/assets/styles/globals.sass" as *;
+          `,
+        },
+      },
+    },
+  },
+});
+
