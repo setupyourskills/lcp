7554def46d811068d27d7d83bb7ad6526e6a2275
Setup Your Skills <setupyourskills@gmail.com>
2026-01-13 19:59:06 +0800

    feat(Testimonial): add content to the section

--- app/components/section/Testimonials.vue
+++ app/components/section/Testimonials.vue
@@ -1,6 +1,15 @@
 <template lang="pug">
 section.testimonials
   ComponentArticleHeader.testimonials__title-component(:title="articleHeader.title" :content="articleHeader.content")
+  div.testimonials__group
+    ComponentTestimonialCard.testimonials__testimonial-card-component(
+      v-for="testimonial in testimonials"
+      :key="testimonial"
+      :content="testimonial.content"
+      :avatar="testimonial.avatar"
+      :name="testimonial.name"
+      :country="testimonial.country"
+    )
 </template>
 
 <script setup lang="ts">
@@ -8,8 +17,39 @@ const articleHeader: ArticleHeader = {
   title: "Les <span class='font-accent'>avis</span> de nos clients",
   content: "Votre <span class='font-bold'>satisfaction</span> est <span class='font-bold'>notre priorité !</span>",
 };
+const testimonials = [
+  {
+    content: "Merci beaucoup pour ces belles couleurs ! Je les utilise maintenant dans toutes mes gravures !",
+    avatar: "",
+    name: "Yang Peng",
+    country: "Taïwan",
+  },
+  {
+    content: "Il faut un petit temps d'adaptation mais ça vaut le coup car ça ajoute une véritable touche vivante à mes produits.",
+    avatar: "",
+    name: "Léa Morelli",
+    country: "France",
+  },
+  {
+    content: "Merci, j'apprécie vraiment les quantités disponibles et bien protégées dans leur boîte, c'est assez facile d'emploi !",
+    avatar: "",
+    name: "John Welds",
+    country: "USA",
+  },
+];
 </script>
+
 <style lang="sass" scoped>
 .testimonials
-  margin-top: $phi3
+  margin-block: $phi3 0
+
+  &__group
+    display: flex
+    flex-direction: column
+    align-items: center
+    gap: $phi1_5
+    margin-inline: auto
+    padding-inline: $phi3
+    margin-top: $phi2
+    max-width: 300px
 </style>
