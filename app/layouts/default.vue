cd23e9642f7338f844cdbde17ecc8fb0066945eb
Setup Your Skills <setupyourskills@gmail.com>
2026-01-14 13:05:49 +0800

    chore(layouts): clean up unused code

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,16 +1,13 @@
 <template lang="pug">
-div.test
-  main.main-content
-    <slot />
-    SectionFooter
+main.main-content
+  <slot />
+  SectionFooter
 </template>
 
 <style lang="sass">
 .main-content
   position: relative
   margin-inline: auto
-  padding-inline: $phi1_5
-  max-width: 1280px
   font-family: "Source Sans 3"
   font-size: $phi1
   line-height: 1.5
