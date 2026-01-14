bb5aec07177626a72d47a44d2700dc0affb3bd89
Setup Your Skills <setupyourskills@gmail.com>
2026-01-14 13:09:10 +0800

    feat(main): add a wrapper container to manage component margins

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,16 @@
 <template lang="pug">
-SectionHero
-SectionFeatures
-SectionVideo
-SectionColors
-SectionPurchase
-SectionFaq
-SectionTestimonials
-SectionNewsletter
+div.main-group
+  SectionHero
+  SectionFeatures
+  SectionVideo
+  SectionColors
+  SectionPurchase
+  SectionFaq
+  SectionTestimonials
+  SectionNewsletter
 </template>
+
+<style lang="sass" scoped>
+.main-group
+  margin-inline: $phi1 
+</style>
