c799c094e7813cb5ef4637f70ab9a9f59e5a2039
Setup Your Skills <setupyourskills@gmail.com>
2026-01-14 18:13:20 +0800

    feat(Hero): add content to the section

--- app/components/section/Hero.vue
+++ app/components/section/Hero.vue
@@ -1,4 +1,31 @@
 <template lang="pug">
 section.hero
-  h1 hero
+  div.hero__group
+    h1.hero__title La Couleur au service de vos gravures
+    p.hero__subtitle Sublimez vos gravures lasers avec nos poudres de couleurs vibrantes, durables et faciles à utiliser.
+    ComponentButton(title="Commander")
+  NuxtImg.hero__image(src="hero.webp")
 </template>
+
+<style lang="sass" scoped>
+.hero 
+  border-bottom: 10px solid $accent2
+
+  &__group
+    position: absolute
+    text-align: center
+    margin-inline: $phi1
+    line-height: 1.2
+
+  &__title
+    margin-block: $phi1 $phi1
+    font-size: $phi2
+
+  &__subtitle
+    margin-block: 0 $phi2
+    font-size: $phi1
+
+  &__image
+    display: block
+    width: 100%
+</style>
