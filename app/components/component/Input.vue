945c4d738d1444fc1b69e4a7fbd07ded56fed7c8
Setup Your Skills <setupyourskills@gmail.com>
2026-01-14 19:02:00 +0800

    feat: create Input component

---
+++ app/components/component/Input.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+input.input-component(:placeholder="props.placeholder")
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  placeholder: {
+    type: String,
+    default: "Default placeholder"
+  },
+})
+</script>
+
+<style lang="sass">
+.input-component
+  padding-block: $phi-1
+  box-sizing: border-box;
+  width: 100%
+  text-indent: $phi1
+  font-size: $phi1_5
+  border: 4px solid $accent2
+  border-radius: 25px
+  background-color: $background-color
+
+  &:focus,
+  &:active
+    border: 4px solid $accent1
+    outline: none
+    background-color: $element-background-color
+
+  &::placeholder
+    font-size: $phi1_5
+    color: $secondary
+</style>
