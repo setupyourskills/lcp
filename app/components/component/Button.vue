4016b6340d0f15b325c5113f384bf9dc896bfccf
Setup Your Skills <setupyourskills@gmail.com>
2026-01-18 19:05:47 +0800

    refactor(Button component): replace 'div' and 'p' elements with the 'Button' tag

--- app/components/component/Button.vue
+++ app/components/component/Button.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
-div.button-component
-  p.button-component__title.font-l {{ props.title }}
+button.button-component.font-l {{ props.title }}
 </template>
 
 <script setup lang="ts">
@@ -13,13 +12,11 @@ const props = withDefaults(defineProps<IButtonProps>(), {
 
 <style lang="sass">
 .button-component
-  text-align: center
-  padding-block: $phi-1
+  padding-inline: $phi3
+  padding-block: $phi1
+  min-width: 240px
+  border: none
   border-radius: 25px
   background-color: $accent1
   box-shadow: 4px 4px 4px 0px $secondary
-  width: 240px
-
-  &__title
-    font-weight: bold
 </style>
