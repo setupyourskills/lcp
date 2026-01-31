850bfaa932834316aaf8018e15ec0d4595cebb3a
Setup Your Skills <setupyourskills@gmail.com>
2026-01-31 13:37:03 +0800

    feat(Video section): add video content

--- app/components/section/Video.vue
+++ app/components/section/Video.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
 section.video
   ComponentArticleHeader(:title="articleHeader.title" :content="articleHeader.content")
+  ComponentVidstack(title="Démonstration" :videoSrc="videoSrc")
 </template>
 
 <script setup lang="ts">
 import type { ArticleHeader } from "~/assets/types/types.d.ts"
+import videoSrc from '~/assets/videos/lcp-demo.mp4';
 
 const articleHeader: ArticleHeader = {
   title: "Les poudres <span class='font-accent'>en Action</span>",
