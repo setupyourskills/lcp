8140320af53a913c499376368be7504ab5f9b6b8
Setup Your Skills <setupyourskills@gmail.com>
2026-02-02 11:11:12 +0800

    refactor(useCookies): add a constant for setting the maximum age of the cookie

--- app/composables/useCookies.ts
+++ app/composables/useCookies.ts
@@ -1,11 +1,12 @@
 import type { YesNo } from "~/assets/types/types.d.ts";
 
 const COOKIES_ACCEPTED_KEY = "cookies_accepted";
+const MAX_AGE = 30 * 24 * 60 * 60;
 
 export const useCookies = () => {
   const cookie = useCookie(COOKIES_ACCEPTED_KEY, {
     default: () => "no",
-    maxAge: 30 * 24 * 60 * 60,
+    maxAge: MAX_AGE,
   });
 
   function setCookie(status: YesNo) {
