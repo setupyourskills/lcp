bf56d788badf4ccdd3de56770111d9f960a7f25c
Setup Your Skills <setupyourskills@gmail.com>
2026-02-11 12:11:01 +0800

    refactor(API): simplify the SQL query for retrieving the view

--- server/api/view/[name].ts
+++ server/api/view/[name].ts
@@ -4,24 +4,22 @@ import db from "../../utils/db";
 export default defineEventHandler(async (event) => {
   const componentName = event.context.params?.name;
 
-  const query = getQuery(event);
-  const sectionName = query.section?.toString();
-  const lang = Array.isArray(query.lang) ? query.lang[0] : (query.lang ?? "fr");
-
   if (!componentName) {
     throw createError({ statusCode: 400, message: "Component name missing" });
   }
 
+  const query = getQuery(event);
+  const lang = Array.isArray(query.lang) ? query.lang[0] : (query.lang ?? "en");
+
   const sql = `
     SELECT *
     FROM  ${mysql.escapeId(componentName)}
-    WHERE name = ? AND lang = ?
+    WHERE lang = ?
   `;
 
   try {
     const [rows] = await db.execute<mysql.RowDataPacket[]>(sql, [
-      sectionName,
-      lang,
+      lang
     ]);
     if (!Array.isArray(rows) || rows.length === 0) {
       throw createError({ statusCode: 404, message: "Section not found" });
