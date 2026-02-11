b159a10dfeed0f622765b216ed78d40f3095a033
Setup Your Skills <setupyourskills@gmail.com>
2026-02-11 15:51:23 +0800

    fix(useComponents composable): convert multiple components into an array for unified handling

--- app/composables/useComponents.ts
+++ app/composables/useComponents.ts
@@ -1,11 +1,23 @@
-import type { KnownComponent, ComponentsMap } from "~/assets/types/types.d.ts"
+import type { KnownComponent, ComponentsMap } from "~/assets/types/types.d.ts";
 
 export function useComponents(blocks: any) {
   const components: ComponentsMap = {};
 
   for (const element of blocks.value) {
     const { component_type, ...rest } = element;
-    components[component_type as KnownComponent] = rest;
+    const key = component_type as KnownComponent;
+
+    if (!(key in components)) {
+      components[key] = rest;
+    } else {
+      const current = components[key];
+
+      if (!Array.isArray(current)) {
+        components[key] = [current as object, rest];
+      } else {
+        (components[key] as any[]).push(rest);
+      }
+    }
   }
 
   return components;
