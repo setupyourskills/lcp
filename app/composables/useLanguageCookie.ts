51c5d827f19dc7ee07dc471e87f8dc49d15844a9
Setup Your Skills <setupyourskills@gmail.com>
2026-02-15 00:07:43 +0800

    refactor(useLanguageCookie): avoid using helper function, prefer variable to retrieve site language state

--- app/composables/useLanguageCookie.ts
+++ app/composables/useLanguageCookie.ts
@@ -1,7 +1,7 @@
 import type { LanguageCookie } from "~/assets/types/types.d.ts";
 
 const PREFIX = "sys-lcp";
-const LANGUAGE_KEY = "language"
+const LANGUAGE_KEY = "language";
 const MAX_AGE = 30 * 24 * 60 * 60;
 
 export const useLanguageCookie = () => {
@@ -18,8 +18,11 @@ export const useLanguageCookie = () => {
     return _languageCookie.value;
   }
 
+  const lang = computed(() => _languageCookie.value);
+
   return {
     setLanguage,
     getLanguage,
+    lang,
   };
 };
