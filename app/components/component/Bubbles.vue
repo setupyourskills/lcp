549be7bb67fa8af42f157b7a1234945bd16bd6ed
Setup Your Skills <setupyourskills@gmail.com>
2026-03-04 15:39:23 +0800

    refactor(Bubbles): improve code structure and syntax for cleaner, more maintainable implementation

--- app/components/component/Bubbles.vue
+++ app/components/component/Bubbles.vue
@@ -3,7 +3,7 @@ div.bubbles-component
   div.bubbles-component__bubble(
     v-for="color in ['green', 'red', 'yellow']"
     :key="color"
-    :class="`bubbles-component--${color}-${props.section}`"
+    :class="`bubbles-component__bubble--${color}-${props.section}`"
   )
 </template>
 
@@ -21,38 +21,33 @@ const props = defineProps<{
     aspect-ratio: 1/1
     border-radius: 50%
 
-  &--green-purchase
-    top: 140px
-    right: -100px
-    background-color: $green-bubble
-    width: 200px
+    &--green-purchase
+      top: 140px
+      right: -100px
+      width: 200px
+      background-color: $green-bubble
 
-    @media screen and (min-width: 900px)
-      position: absolute
-      top: 540px
-      left: -130px
+      @media screen and (min-width: 900px)
+        position: absolute
+        top: 540px
+        left: -130px
 
-  &--red-purchase
-    top: -160px
-    left: -240px
-    width: 330px
-    background-color: $red-bubble
+    &--red-purchase,
+    &--red-footer
+      top: -160px
+      left: -240px
+      width: 330px
+      background-color: $red-bubble
 
-  &--green-footer
-    top: 200px
-    right: -120px
-    background-color: $green-bubble
-    width: 200px
+    &--green-footer
+      top: 200px
+      right: -120px
+      width: 200px
+      background-color: $green-bubble
 
-  &--red-footer
-    top: -160px
-    left: -240px
-    width: 330px
-    background-color: $red-bubble
-
-  &--yellow-footer
-    bottom: -125px
-    left: -250px
-    width: 330px
-    background-color: $yellow-bubble
+    &--yellow-footer
+      bottom: -125px
+      left: -250px
+      width: 330px
+      background-color: $yellow-bubble
 </style>
