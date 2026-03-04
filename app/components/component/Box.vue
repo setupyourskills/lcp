9d26c0e74bfaee344169cfd312086a34a1c2fd0f
Setup Your Skills <setupyourskills@gmail.com>
2026-03-04 15:31:57 +0800

    refactor(Box component): optimize and improve code structure and syntax for cleaner, more maintainable implementation

--- app/components/component/Box.vue
+++ app/components/component/Box.vue
@@ -4,7 +4,7 @@ div.box-component(
   :class="{ 'activate-element': getPspState(props.boxName)}"
 )
   div.box-component__logo
-    NuxtImg.box-component__logo-img(:src="`${props.boxName}.png`")
+    NuxtImg(:src="`${props.boxName}.webp`")
 </template>
 
 <script setup lang="ts">
@@ -17,33 +17,28 @@ const { getPspState, setPspState } = usePspsState();
 
 <style lang="sass" scoped>
 .box-component
+  @include transition
   display: flex
   justify-content: center
   align-items: center
-  margin-block: 0
-  padding-inline: $phi1
-  width: 160px
-  height: 62px
+  padding: $phi1
+  min-width: 200px
   border-radius: 15px
   border-bottom: 6px solid $accent2
   background-color: $second-background-color
   cursor: pointer
-  transition: border-bottom .4s ease-out, background-color .2s ease-out, transform .2s ease-out, box-shadow .25s ease-out
-
-  &:hover
-    transition: border-bottom .4s ease-out, background-color .2s ease-out, transform .2s ease-out, box-shadow .25s ease-out
-    box-shadow: 4px 4px 4px 0px $secondary
-    transform: translate(-3px, -3px)
 
   &__logo
     display: flex
     justify-content: center
     align-items: center
-    padding: $phi-2
 
-  &__logo-img
-    height: 25px
+    img
+      height: $phi1_5
 
-  &__content
-    line-height: $phi1_5
+.activate-element
+  cursor: default
+
+  &:hover
+    border-color: $accent2
 </style>
