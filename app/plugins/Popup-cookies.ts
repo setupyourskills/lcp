3345c386e8929864fe06eafd02f80baa5dac2445
Setup Your Skills <setupyourskills@gmail.com>
2026-03-06 21:15:46 +0800

    refactor(Popup-cookies plugin): relocate shared constant definitions into the project’s global file

--- app/plugins/Popup-cookies.ts
+++ app/plugins/Popup-cookies.ts
@@ -1,8 +1,6 @@
-const COOKIE_POPUP_TIMEOUT = 1500;
-
 export default defineNuxtPlugin(() => {
   const { setPopupState } = usePopupsState();
-  const { COOKIES_CAT_KEYS, isCookieAccepted, setCookie } = useCookies();
+  const { isCookieAccepted, setCookie } = useCookies();
 
   if (!isCookieAccepted.value) {
     for (const key of COOKIES_CAT_KEYS) {
