c6377fcdc9578ba64313ca7075f8ed1e5c12fca8
Setup Your Skills <setupyourskills@gmail.com>
2026-03-06 12:41:05 +0800

    refactor(useModalClickHandler composable): improve code structure and syntax for cleaner, more maintainable implementation

--- app/composables/useModalClickHandler.ts
+++ app/composables/useModalClickHandler.ts
@@ -1,38 +1,39 @@
-const CLOSE_MODAL_TIMEOUT = 400;
-
 export const useModalClickHandler = () => {
   const { setModalState } = useModalsState();
 
-  const clickHandler = (e: MouseEvent, isInModal: boolean, modalName?: keyof IModalsState) => {
+  const clickHandler = (
+    e: MouseEvent,
+    isInModal: boolean,
+    modalName?: keyof IModalsState,
+  ) => {
     const target = e.target as HTMLElement | null;
-
     if (!target) return;
 
     const href = target.getAttribute("href");
     if (!href) return;
 
+    e.preventDefault();
+
     if (target.classList.contains("modal")) {
       const modalKey = href.slice(1) as keyof IModalsState;
-
-      e.preventDefault();
 
       if (isInModal && modalName) {
         setModalState(modalName as keyof IModalsState, false);
 
         setTimeout(() => {
           setModalState(modalKey, true);
-        }, CLOSE_MODAL_TIMEOUT);
+        }, OPEN_MODAL_TIMEOUT);
       } else setModalState(modalKey, true);
-
     } else if (target.classList.contains("inner-link")) {
-    const targetId = href.slice(1);
-    const targetEl = document.getElementById(targetId);
+      const targetId = href.slice(1);
+      const targetEl = document.getElementById(targetId);
 
-    e.preventDefault();
-
-    if (targetEl) targetEl.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  }
-
+      if (targetEl)
+        targetEl.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+    }
   };
   return {
     clickHandler,
