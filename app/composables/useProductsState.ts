b28b85fe5a07f99e5eb4505a7172bb1b6f2e035a
Setup Your Skills <setupyourskills@gmail.com>
2026-03-06 11:13:45 +0800

    refactor(useProductssState composable): improve code structure and syntax for cleaner, more maintainable implementation

--- app/composables/useProductsState.ts
+++ app/composables/useProductsState.ts
@@ -1,17 +1,18 @@
 export const useProductsState = () => {
-  const _productState = useState<{products: IProduct[]}>("stateProducts", () => ({
-    products: [],
-  }));
+  const _productState = useState<{ products: IProduct[] }>(
+    "stateProducts",
+    () => ({
+      products: [],
+    }),
+  );
 
   function setAllProducts(newProducts: IProduct[]) {
     _productState.value.products = newProducts;
   }
 
   function getProduct(color: keyof ICounterColorsCookie) {
-    if (!color) return;
-
     return _productState.value.products.find(
-      (p: IProduct) => p.color === color
+      (p: IProduct) => p.color === color,
     );
   }
 
