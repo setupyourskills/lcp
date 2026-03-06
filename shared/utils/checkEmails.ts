d12b81e98e9e91346085d0b587527d8e0ef799df
Setup Your Skills <setupyourskills@gmail.com>
2026-03-06 13:43:11 +0800

    chore(CheckEmail): relocate utility functions to /shared/utils for better project organization

--- shared/utils/checkEmails.ts
+++ shared/utils/checkEmails.ts
@@ -1,8 +1,11 @@
 const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-export const useCheckEmail = (email: string = "") => {
+export const checkEmail = (email: string = "") => {
   const isInvalidRegex = !EMAIL_REGEX.test(email);
   const isTooLong = email.length > 255;
 
-  return { isInvalidRegex, isTooLong };
+  return {
+    isInvalidRegex,
+    isTooLong,
+  };
 };
