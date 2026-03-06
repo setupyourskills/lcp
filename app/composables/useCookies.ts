4a8e609fafe72c7a14c513aeaf857e463174352d
Setup Your Skills <setupyourskills@gmail.com>
2026-03-06 12:31:18 +0800

    refactor(useCookies composable): improve code structure and syntax for cleaner, more maintainable implementation

--- app/composables/useCookies.ts
+++ app/composables/useCookies.ts
@@ -1,29 +1,25 @@
-const PREFIX = "sys-lcp";
-const MAX_AGE = 30 * 24 * 60 * 60;
-
-const COOKIES_ACCEPTED_KEY = "consent_done";
-const COOKIES_CAT_KEYS = [
-  "functional",
-  "advertising",
-  "analytics",
-];
-
 export const useCookies = () => {
   const _cookie = (cat: string) =>
-    useCookie(`${PREFIX}_${cat}`, {
+    useCookie(`${COOKIES_PREFIX}_${cat}`, {
       default: () => "no",
-      maxAge: MAX_AGE,
+      maxAge: COOKIES_MAX_AGE,
     });
-
-  function setCookie(cat: string, status: boolean) {
-    _cookie(cat).value = status ? "yes" : "no";
-  }
 
   function getCookie(cat: string) {
     return _cookie(cat).value === "yes";
   }
 
-  const isCookieAccepted = computed(() => (_cookie(COOKIES_ACCEPTED_KEY).value === "yes"));
+  function setCookie(cat: string, status: boolean) {
+    _cookie(cat).value = status ? "yes" : "no";
+  }
 
-  return { COOKIES_ACCEPTED_KEY, COOKIES_CAT_KEYS, getCookie, setCookie, isCookieAccepted };
+  const isCookieAccepted = computed(
+    () => _cookie(COOKIES_ACCEPTED_KEY).value === "yes",
+  );
+
+  return {
+    getCookie,
+    setCookie,
+    isCookieAccepted,
+  };
 };
