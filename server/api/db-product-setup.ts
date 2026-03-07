1b1f374eaf175d756d205ab78d7aad479eb6afb5
Setup Your Skills <setupyourskills@gmail.com>
2026-03-07 12:08:00 +0800

    refactor(db-product-setup api): improve code structure and syntax for cleaner, more maintainable implementation

--- server/api/db-product-setup.ts
+++ server/api/db-product-setup.ts
@@ -2,13 +2,13 @@ import mysql from "mysql2/promise";
 import { db } from "../utils/db";
 
 export default defineEventHandler(async () => {
-  const sql = `
+  const PRODUCT_QUERY = `
     SELECT id, color, name, description, price
     FROM products
   `;
 
  try {
-    const [rows] = await db.execute<mysql.RowDataPacket[]>(sql);
+    const [rows] = await db.execute<mysql.RowDataPacket[]>(PRODUCT_QUERY);
 
     return rows as IProduct[];
   } catch (err: any) {
