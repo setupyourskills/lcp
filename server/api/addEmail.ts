b2a44eacbf84fe549779141e36d360f1dd899ea4
Setup Your Skills <setupyourskills@gmail.com>
2026-03-07 12:06:07 +0800

    feat(Addmail api): strengthen email validation and refactor related code to improve data integrity and maintainability

--- server/api/addEmail.ts
+++ server/api/addEmail.ts
@@ -4,20 +4,22 @@ import { db } from "../utils/db";
 export default defineEventHandler(async (event) => {
   const { email } = await readBody(event);
 
-  if (!email || typeof email !== "string") {
-    throw createError({ statusCode: 400, message: "Invalid e‑mail" });
+  const { isInvalidRegex, isTooLong } = checkEmail(email.value);
+
+  if (!email || typeof email !== "string" || isInvalidRegex || isTooLong) {
+    throw createError({ statusCode: 400, message: "Invalid e‑mail!" });
   }
 
-  const sql = "INSERT INTO users (Email) VALUES (?)";
+  const EMAIL_QUERY = "INSERT INTO users (Email) VALUES (?)";
 
   try {
-    const [result] = await db.execute<mysql.ResultSetHeader>(sql, [email]);
+    const [result] = await db.execute<mysql.ResultSetHeader>(EMAIL_QUERY, [email]);
 
     console.log(result.insertId, email);
   } catch (err: any) {
     throw createError({
       statusCode: 500,
-      message: 'Database insertion failed',
+      message: "Database insertion failed!",
       data: { err },
     });
   }
